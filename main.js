2246d633ac8eb6518d9fc850a288b00c487817e6
Deepankar Agrawal <deepankar01agrawal@gmail.com>
2017-07-17 16:14:12 +0530

    :bug: removed the menu bar

--- main.js
+++ main.js
@@ -34,6 +34,7 @@ function createWindow() {
 
   // Open the DevTools.
   win.webContents.openDevTools()
+   win.setMenuBarVisibility(false);
 
   // Emitted when the window is closed.
   win.on('closed', () => {
