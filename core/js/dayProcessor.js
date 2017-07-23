efe8fb1315504af7b9ad8172b758f25ec90aaf8e
Deepankar Agrawal <deepankar01agrawal@gmail.com>
2017-07-23 18:35:09 +0530

    added gaurd condition if "A" then only accept leaves and Regularization

--- core/js/dayProcessor.js
+++ core/js/dayProcessor.js
@@ -7,76 +7,87 @@ const extractADay = function () {
     this.getEmployee = (employeeBiometricData) => {
         this.employeeDetails = employeeBiometricData;
         return this;
-    },
+    }
     this.adjustSaturdayAsWeekOf = () => {
         //extract all saturdays of the month
-         let firstDay = moment(Object.keys(this.employeeDetails)[3], "DD-MMM").set("year", moment().get("year"));
-         for (let i = 0; i < firstDay.daysInMonth(); i++) {
-            if(moment(firstDay).add(i, "days").format("dd") === "Sa"){
-                if(this.employeeDetails[moment(firstDay).add(i, "days").format("DD-MMM")] === "A") 
+        let firstDay = moment(Object.keys(this.employeeDetails)[3], "DD-MMM").set("year", moment().get("year"));
+        for (let i = 0; i < firstDay.daysInMonth(); i++) {
+            if (moment(firstDay).add(i, "days").format("dd") === "Sa") {
+                if (this.employeeDetails[moment(firstDay).add(i, "days").format("DD-MMM")] === "A")
                     this.employeeDetails[moment(firstDay).add(i, "days").format("DD-MMM")] = "WO";
-                    this.employeeDetails["WO"] = parseInt(this.employeeDetails["WO"],10)+1;
+                this.employeeDetails["WO"] = parseInt(this.employeeDetails["WO"], 10) + 1;
             }
-         }
+        }
         return this;
     }
     this.extractAbsents = () => {
-            this.absentDates = [];
-            let firstDay = moment(Object.keys(this.employeeDetails)[3], "DD-MMM").set("year", moment().get("year"));
-            for (let i = 0; i < firstDay.daysInMonth(); i++) {
-                if(this.employeeDetails[moment(firstDay).add(i, "days").format("DD-MMM")] === "A") 
-                    this.absentDates.push(moment(firstDay).add(i, "days").format("DD-MMM"));
-            }
-            return this;
-    },
+        this.absentDates = [];
+        let firstDay = moment(Object.keys(this.employeeDetails)[3], "DD-MMM").set("year", moment().get("year"));
+        for (let i = 0; i < firstDay.daysInMonth(); i++) {
+            if (this.employeeDetails[moment(firstDay).add(i, "days").format("DD-MMM")] === "A")
+                this.absentDates.push(moment(firstDay).add(i, "days").format("DD-MMM"));
+        }
+        return this;
+    }
     this.extractRegularizedData = (regularizedDataJSON) => {
         //get all the regularization of the month for the employee
         this.regularizedData = _.without(_.map(regularizedDataJSON, (regularization) => this.employeeDetails.id === regularization.id ? regularization : null), null);
         return this;
-    },
+    }
     this.fillReguluraizedData = () => {
-            //change the status of the particular date with the status of the day as P
-            if (!_.isEmpty(this.regularizedData)) {
-                this.regularizedData.forEach((regularization) => {
-                    let from = moment(regularization.from, "DD-MMM-YY");
-                    let to = moment(regularization.to, "DD-MMM-YY");
-                    for (let i = 0; i <= to.diff(from, "days"); i += 1) {
+        //change the status of the particular date with the status of the day as P
+        if (!_.isEmpty(this.regularizedData)) {
+            this.regularizedData.forEach((regularization) => {
+                let from = moment(regularization.from, "DD-MMM-YY");
+                let to = moment(regularization.to, "DD-MMM-YY");
+                for (let i = 0; i <= to.diff(from, "days"); i += 1) {
+                    if (this.employeeDetails[moment(from).add(i, "days").format("DD-MMM")] === "A") {
                         this.employeeDetails[moment(from).add(i, "days").format("DD-MMM")] = selectRegularizedReason(regularization.category);
                         //delete a particular date for which it was regularized
                         delete this.absentDates[this.absentDates.indexOf(moment(from).add(i, "days").format("DD-MMM"))];
                         this.employeeDetails.P = parseInt(this.employeeDetails.P, 10) + 1;
                     }
-                });
-            };
-            return this;
-    },
+                    else {
+                        this.employeeDetails[moment(from).add(i, "days").format("DD-MMM")] = "ERROR"
+                    }
+
+                }
+            });
+        };
+        return this;
+    }
     this.extractLeaveRequestData = (leaveRequestDataJSON) => {
-            this.leaveRequestData = _.without(_.map(leaveRequestDataJSON, (leaveRequest) => this.employeeDetails.id === leaveRequest.id ? leaveRequest : null), null);
-            return this;
-    },
+        this.leaveRequestData = _.without(_.map(leaveRequestDataJSON, (leaveRequest) => this.employeeDetails.id === leaveRequest.id ? leaveRequest : null), null);
+        return this;
+    }
     this.fillLeaveRequestData = () => {
         if (!_.isEmpty(this.leaveRequestData)) {
             this.leaveRequestData.forEach((leaveRequest) => {
                 let from = moment(leaveRequest.from, "DD-MMM-YY");
                 let to = moment(leaveRequest.to, "DD-MMM-YY");
                 for (let i = 0; i <= to.diff(from, "days"); i += 1) {
-                    this.employeeDetails[moment(from).add(i, "days").format("DD-MMM")] = selectLeaveRequestReason(leaveRequest.category);
-                    delete this.absentDates[this.absentDates.indexOf(moment(from).add(i, "days").format("DD-MMM"))];
-                    this.employeeDetails.A = parseInt(this.employeeDetails.A, 10) + 1;
+                    if (this.employeeDetails[moment(from).add(i, "days").format("DD-MMM")] === "A") {
+                        this.employeeDetails[moment(from).add(i, "days").format("DD-MMM")] = selectLeaveRequestReason(leaveRequest.category);
+                        delete this.absentDates[this.absentDates.indexOf(moment(from).add(i, "days").format("DD-MMM"))];
+                        this.employeeDetails.A = parseInt(this.employeeDetails.A, 10) + 1;
+                    }
+                    else {
+                        this.employeeDetails[moment(from).add(i, "days").format("DD-MMM")] = "ERROR"
+                    }
                 }
             });
         }
         return this;
-    },
+    }
     this.fillErrors = () => {
-            this.absentDates.forEach((absent)=>{
-                this.employeeDetails[absent] = "ERROR";
-            })
-            return this;
-    },    
+        this.absentDates.forEach((absent) => {
+            this.employeeDetails[absent] = "ERROR";
+        })
+        return this;
+    }
     this.finalizeDay = () => {
-            console.log(this.employeeDetails)
-            return this;
+        console.log(this.employeeDetails)
+        return this;
     }
 
     if (this instanceof extractADay) {
@@ -110,18 +121,18 @@ const selectRegularizedReason = (category) => {
             break;
         case "Travel on Duty":
             status = "TRAVEL";
-            break;            
+            break;
         case "Had forgotten to Login":
             status = "FORGOTLOGIN";
             break;
         case "Will be on Training":
         case "Was on Training":
             status = "TRAINING";
-            break;           
-        case "Will be on Duty/Official Tour": 
+            break;
+        case "Will be on Duty/Official Tour":
         case "Was on Duty/Official Tour":
             status = "ONSITE";
-            break;  
+            break;
         default:
             status = "ERROR";
     }
