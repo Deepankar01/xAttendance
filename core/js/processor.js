bfeefe6a6c0349f7996eab6cc9d0b2b525008d39
Deepankar Agrawal <deepankar01agrawal@gmail.com>
2017-07-23 18:30:39 +0530

    comments change

--- core/js/processor.js
+++ core/js/processor.js
@@ -2,13 +2,12 @@ const moment = require("moment");
 const {extractADay} = require("./dayProcessor");
 /**
  * Callee of the function
- * @param {*} regularizedDataJSON 
- * @param {*} leaveRequestDataJSON 
- * @param {*} biometricDataJSON 
+ * @param {string} regularizedDataJSON 
+ * @param {string} leaveRequestDataJSON 
+ * @param {string} biometricDataJSON 
  */
 const processExcels = (regularizedDataJSON, leaveRequestDataJSON, biometricDataJSON) => {
-    //get first date of the month
-    // let firstDay = moment(Object.keys(biometricDataJSON[0])[3],"DD-MMM").set("year",moment().get("year"));
+    //process employee by employee 
     for(let i=0;i< biometricDataJSON.length;i+=1){
         new extractADay()
         .getEmployee(biometricDataJSON[i])
